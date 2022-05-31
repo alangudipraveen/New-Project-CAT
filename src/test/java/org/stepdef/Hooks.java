102d392196be8ffd6c9d2d6111d20ec7d5938fa7
alangudipraveen <praveen.spk189@gmail.com>
2022-05-31 13:18:38 +0530

    new person updated

--- src/test/java/org/stepdef/Hooks.java
+++ src/test/java/org/stepdef/Hooks.java
@@ -22,6 +22,14 @@ public class Hooks extends BaseClass {
 	}
 	@After
 	public void afterExecution(Scenario s) {
+		System.out.println("trail1");
+		System.out.println("trail2");
+		System.out.println("trail3");
+		System.out.println("trail4");
+		System.out.println("trail5");
+		System.out.println("trail6");
+		System.out.println("trail7");
+		System.out.println("trail8");
 		if (s.isFailed()) {
 			TakesScreenshot ts = (TakesScreenshot) driver;
 			byte[] screenshotAs = ts.getScreenshotAs(OutputType.BYTES);
